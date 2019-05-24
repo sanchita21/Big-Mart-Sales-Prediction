5cdf9844f98373ac72c81bbc78ae09fd1fd01df3
sanchita21 <50104474+sanchita21@users.noreply.github.com>
2019-05-24 12:55:46 +0530

    Update Bigmart.r

--- Bigmart.r
+++ Bigmart.r
@@ -34,3 +34,64 @@ train_bigmart%>%
             group_by(Item_Type,Outlet_Identifier)%>%
             summarise(sum_sales=sum(Item_Outlet_Sales))%>%arrange(desc(sum_sales))
 # outlet 27 has the highest sales i.e of fruits and veggies , snack food as well as household
+
+# we will add training and test data for data cleaning,visualization and data prep purpose
+# for adding purpose both the dataset should have same number of col so we will add a col to the test data containing NA valuestest[,Item_Outlet_Sales := NA
+test_bigmart<-read.csv("C:\\Users\\xeadmin\\Desktop\\Big Mart Sales Prediction\\Test_BigMart.csv")
+str(test_bigmart)
+test_bigmart$Item_Outlet_Sales<-NA
+
+
+# appending the training and the test for data cleaning and prep
+master<-rbind(train_bigmart,test_bigmart)
+dim(master)
+summary(is.na(master))
+
+
+
+hist(master$Item_Outlet_Sales)
+# if we look at the hist we find that data is skewed to the right.we have greater mean thn the median
+# we can use log to normalize the data
+
+hist(master$Item_Weight)
+#we see that the weights of the product are not much skewed .
+hist(master$Item_Visibility)
+# item visibility is also right skewed which means mean is greater thn the median
+
+library(ggplot2)
+ggplot(master,aes(Outlet_Identifier,Item_Weight))+geom_boxplot()
+# we find that only the weight of outlet 19 and outlet 27 is missing
+
+ggplot(master,aes(Item_Type,Item_Weight))+geom_boxplot()
+#we find that item weights for all the item types are available.
+
+ggplot(master,aes(Item_Type,Item_Visibility))+geom_boxplot()
+# the dots show the outliers for each item type
+
+ggplot(master,aes(x=Item_Outlet_Sales,Item_Visibility,col=Item_Type))+ geom_point()
+
+
+ggplot(master,aes(x=Item_Outlet_Sales,Item_Visibility,col=Outlet_Location_Type))+ geom_point()
+
+
+
+ggplot(master,aes(y=Item_Outlet_Sales,x=Item_MRP,col=Outlet_Location_Type))+ geom_point()
+#there are 4 categories in which mrp is divided
+# *************Why could be the possible reasons for this?
+
+ggplot(master,aes(Outlet_Type))+geom_bar(stat="count")
+# outlet type (sypermarket 1) has the highhest number.so it is the most popular.
+
+ggplot(master,aes(Outlet_Establishment_Year))+geom_bar(stat="count")
+#No outlets were established in the year 1987 to 1996...In 1998 lowest number of outlets were established
+
+
+
+
+
+
+
+
+
+
+
